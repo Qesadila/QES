c3dc3dc041766621b0fd446bb3e882e36801e170
Patrik Procházka <>
2020-04-05 14:07:28 +0200

    login / register

--- src/main.js
+++ src/main.js
@@ -1,31 +1,35 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import router from './router'
+import Vue from "vue";
+import "./plugins/vuetify";
+import router from "./router";
 
-import BootstrapVue from "bootstrap-vue"
+import BootstrapVue from "bootstrap-vue";
 
-import App from './App'
+import App from "./App";
 
-import Default from './Layout/Wrappers/baseLayout.vue';
-import Pages from './Layout/Wrappers/pagesLayout.vue';
-import Apps from './Layout/Wrappers/appLayout.vue';
-import Axios from 'axios'
-import i18n from "./i18n"
+import Default from "./Layout/Wrappers/baseLayout.vue";
+import Pages from "./Layout/Wrappers/pagesLayout.vue";
+import Apps from "./Layout/Wrappers/appLayout.vue";
+import Axios from "axios";
+import i18n from "./i18n";
 
 Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue);
 
-Vue.component('default-layout', Default);
-Vue.component('userpages-layout', Pages);
-Vue.component('apps-layout', Apps);
+Vue.component("default-layout", Default);
+Vue.component("userpages-layout", Pages);
+Vue.component("apps-layout", Apps);
 
-Vue.prototype.$http = Axios
+Vue.prototype.$http = Axios;
+const token = localStorage.getItem("token");
+if (token) {
+  Vue.prototype.$http.defaults.headers.common["Authorization"] = token;
+}
 
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   i18n,
-  template: '<App/>',
-  components: { App }
+  template: "<App/>",
+  components: { App },
 });
