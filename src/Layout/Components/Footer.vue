aa17a45b486de6db684e734746f55c2fea61edc2
Ludovit Scholtz <ludovit.scholtz@wood.cz>
2020-04-07 11:34:28 +0200

    footer localisation

--- src/Layout/Components/Footer.vue
+++ src/Layout/Components/Footer.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="app-footer">
         <v-row>
-            <div class="col-12">Qesadila is open source voting system which uses the Qualified Electronic Signature for vote verifications. <a href="https://www.github.com/Qesadila/QES" target="_blank">https://www.github.com/Qesadila/QES</a></div>
+            <div class="col-12">
+                {{ $t('footerText') }}
+                <a href="https://www.github.com/Qesadila/QES" target="_blank">https://www.github.com/Qesadila/QES</a>
+            </div>
         </v-row>
     </div>
 </template>
