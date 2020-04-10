53a242d5c91734b4c8052152b01d11343d666ab1
martincapek <me@martincapek.cz>
2020-04-10 17:02:49 +0200

    add users in voting list

--- pages/voting-list-manager/form/create.vue
+++ pages/voting-list-manager/form/create.vue
@@ -5,132 +5,95 @@
         <div class="d-flex flex-row justify-center py-5 display-1">
           Create New Voter List
         </div>
-        <div class="d-flex flex-row justify-center px-12 mb-6 body">
-          {{ mockForm.infoText }}
-        </div>
+
         <v-divider class="mb-10"></v-divider>
-        <div
-          v-for="(question, index) in mockForm.questions"
-          :key="question.id"
-          class="px-12"
-        >
-          <div class="body-2 mb-2">Question {{ index + 1 }}</div>
-          <div class="title">{{ question.text }}</div>
-          <div class="pl-5">
-            <v-radio-group
-              @change="handleUserAnswerChange(question.id, $event)"
-            >
-              <v-radio
-                v-for="option in question.options"
-                :key="option.id"
-                :label="option.text"
-                :value="option.value"
-                class="my-2"
-              >
-              </v-radio>
-              <!-- Last option is always with value null as user dont want to answer this question -->
-              <v-radio
-                class="my-2"
-                label="I do not want to answer"
-                value="N/A"
-              ></v-radio>
-            </v-radio-group>
-            <v-divider class="mb-10"></v-divider>
-          </div>
+
+        <div class="px-5">
+          <v-text-field v-model="listName" label="List name" outlined="" />
         </div>
+
         <div class="d-flex flex-row justify-center px-12 mb-6 body">
-          {{ mockForm.endingText }}
+          <v-btn x-large color="primary" @click="addUsers">Manage users</v-btn>
         </div>
-        <div class="d-flex flex-row justify-center px-12 mb-6 body">
-          <v-btn x-large color="primary" @click="submitForm">Send form</v-btn>
+
+        <div class="pa-5" v-if="listId !== null">
+          <h2 class="mb-3 text-center">Voters</h2>
+          <v-data-table
+            :headers="headers"
+            :items="voters"
+            :items-per-page="-1"
+            :hide-default-footer="true"
+            class="elevation-1"
+          >
+          </v-data-table>
+
+          <v-dialog v-model="dialog" width="500">
+            <template v-slot:activator="{ on }">
+              <v-btn color="red lighten-2" dark v-on="on">
+                add user
+              </v-btn>
+            </template>
+
+            <v-card>
+              <v-card-title class="headline grey lighten-2" primary-title>
+                Add user
+              </v-card-title>
+
+              <v-card-text class="pt-5">
+                <v-text-field
+                  v-model="user.email"
+                  label="User email"
+                  outlined
+                />
+                <v-file-input v-model="user.file" label="Sign file" outlined />
+              </v-card-text>
+
+              <v-divider></v-divider>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" text @click="createAndAttachUser">
+                  Add user
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         </div>
       </form>
     </v-card>
   </v-row>
 </template>
 <script>
-const mockForm = {
-  id: 'ASkjahdf24',
-  title: 'This is mock form',
-  infoText:
-    'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nullam feugiat, turpis at pulvinar vulputate, erat libero tristique tellus, nec bibendum odio risus sit amet ante. In sem justo, commodo ut, suscipit at, pharetra vitae, orci. Nulla est. Pellentesque sapien. Pellentesque ipsum. Integer rutrum, orci vestibulum ullamcorper ultricies, lacus quam ultricies odio, vitae placerat pede sem sit amet enim. Aenean placerat. Integer pellentesque quam vel velit. Pellentesque arcu.',
-  endingText: 'Thank you for participating. Your honesty, major',
-  questions: [
-    {
-      id: 1,
-      text: 'This is mock qeustion in mock form for mock user.',
-      type: 'one',
-      options: [
-        {
-          id: 1,
-          text: 'Mock answer 1',
-          value: 1
-        },
-        {
-          id: 2,
-          text: 'Mock answer 2',
-          value: 2
-        },
-        {
-          id: 3,
-          text: 'Mock answer 3',
-          value: 3
-        }
-      ]
-    },
-    {
-      id: 2,
-      text: 'This is mock qeustion in mock form for mock user.',
-      type: 'multiple',
-      options: [
-        {
-          id: 4,
-          text: 'Mock answer 4',
-          value: 4
-        },
-        {
-          id: 5,
-          text: 'Mock answer 5',
-          value: 5
-        },
-        {
-          id: 6,
-          text: 'Mock answer 6',
-          value: 6
-        }
-      ]
-    },
-    {
-      id: 3,
-      text: 'This is mock qeustion in mock form for mock user.',
-      type: 'text',
-      options: [
-        {
-          id: 7,
-          text: 'Mock answer 7',
-          value: 7
-        },
-        {
-          id: 8,
-          text: 'Mock answer 8',
-          value: 8
-        },
-        {
-          id: 9,
-          text: 'Mock answer 9',
-          value: 9
-        }
-      ]
-    }
-  ]
-}
-
 export default {
   middleware: 'authenticated',
   data() {
     return {
-      mockForm,
-      userAnwers: {}
+      headers: [
+        {
+          text: 'E-mail',
+          sortable: false,
+          value: 'email'
+        },
+        {
+          text: 'Signed Document',
+          sortable: false,
+          value: 'signed_document'
+        },
+        {
+          text: 'Action',
+          sortable: false,
+          value: 'actions',
+          width: 300
+        }
+      ],
+      listName: '',
+      listId: null,
+      voters: [],
+      dialog: false,
+      user: {
+        email: '',
+        file: null
+      }
     }
   },
   methods: {
@@ -139,6 +102,22 @@ export default {
     },
     handleUserAnswerChange(questionId, answerId) {
       this.userAnwers[questionId] = answerId
+    },
+    addUsers() {
+      console.log('add users')
+      this.listId = 20
+    },
+    createAndAttachUser() {
+      this.voters.push({
+        name: this.user.email,
+        file: this.user.file
+      })
+
+      this.user.email = ''
+      this.user.file = null
+      this.dialog = false
+
+      console.log(this.voters)
     }
   }
 }
