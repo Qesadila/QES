cf8536030dbb32d8f6c2a3a8830949863d4b14f3
scholtz <ludko2@gmail.com>
2020-04-10 23:41:35 +0000

    Deploying to master from  @ 4e7ac116f896125aeaec0ac077d7f751741785e4 🚀

---
+++ docs/_nuxt/cdf8713df7007db1b348.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{495:function(t,e,n){"use strict";n.r(e);var h={},l=n(60),component=Object(l.a)(h,(function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("h1",[this._v(this._s(this.$t("welcome")))]),this._v("\n\n  "+this._s(this.$store.state.auth.auth)+"\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
