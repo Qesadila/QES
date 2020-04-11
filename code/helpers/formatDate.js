059c4e48b0537a232da1bbece8f11d7323c1ad71
martincapek <me@martincapek.cz>
2020-04-11 13:45:03 +0200

    add list of votings

--- code/helpers/formatDate.js
+++ code/helpers/formatDate.js
@@ -1,7 +1,11 @@
-import { format } from 'date-fns'
+import { format, compareAsc } from 'date-fns'
 
 export function formatDate(date) {
   const dateObj = new Date(date)
 
   return format(dateObj, 'dd. MM. yyyy HH:mm')
+}
+
+export function isAfter(dateLeft, dateRight) {
+  return compareAsc(new Date(dateLeft), new Date(dateRight)) <= 0
 }
