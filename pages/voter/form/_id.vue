ab4713456bfe8d85186798389be583835e6ba90e
martincapek <me@martincapek.cz>
2020-04-11 14:49:39 +0200

    add voting and signing forms

--- pages/voter/form/_id.vue
+++ pages/voter/form/_id.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row no-gutters class="fill-height pa-12" justify="center" align="center">
-    <v-card v-if="selectedForm" width="100%">
+    <v-card v-if="selectedForm && appRunning" width="100%">
       <form>
         <div class="d-flex flex-row justify-center py-5 display-1">
           {{ selectedForm.title }}
@@ -18,7 +18,9 @@
           <div class="title">{{ question.question }}</div>
           <div class="pl-5">
             <v-radio-group
-              @change="handleUserAnswerChange(question.id, $event)"
+              @change="
+                handleUserAnswerChange(question.votingFormItemId, $event)
+              "
             >
               <v-radio
                 v-for="option in question.votingFormItemOptions"
@@ -48,17 +50,47 @@
     <div v-else>
       <v-progress-circular indeterminate size="64"></v-progress-circular>
     </div>
+
+    <v-dialog persistent :value="!appRunning" width="500">
+      <v-card>
+        <v-card-title class="headline grey lighten-2" primary-title>
+          Sign app not detected
+        </v-card-title>
+
+        <v-card-text class="pa-5">
+          Please start our sign app to procced on voting
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="warning" to="/voter">Go back</v-btn>
+        </v-card-actions>
+
+        <v-divider></v-divider>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog persistent :value="waitingForSign" width="500">
+      <v-card>
+        <v-card-title class="headline grey lighten-2" primary-title>
+          Please sign your votes in app
+        </v-card-title>
+      </v-card>
+    </v-dialog>
   </v-row>
 </template>
 <script>
 import { mapActions } from 'vuex'
+import { HubConnectionBuilder } from '@microsoft/signalr'
 
 export default {
   middleware: 'authenticated',
   data() {
     return {
       userAnwers: {},
-      allLists: []
+      allLists: [],
+      appRunning: false,
+      waitingForSign: false
     }
   },
   computed: {
@@ -73,11 +105,57 @@ export default {
   },
   mounted() {
     this.fetchList()
+
+    this.connection = new HubConnectionBuilder()
+      .withUrl('http://localhost:58080/hubs/signin')
+      .build()
+
+    this.connection
+      .start()
+      .then(() => {
+        this.appRunning = true
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+
+    this.connection.on('Connecting', () => {
+      alert('connecting')
+    })
+
+    this.connection.on('Authenticate', (cert) => {
+      const fd = new FormData()
+      fd.append('base64message', cert)
+      this.$axios
+        .post('v1/Voter/SubmitVote', fd)
+        .then(() => {
+          this.waitingForSign = false
+          this.$router.push('/voter')
+          this.$store.dispatch(
+            'snackbar/openSuccess',
+            'Your votes was signed and counted!'
+          )
+        })
+        .catch((e) => {
+          this.waitingForSign = false
+          this.$store.dispatch('snackbar/openError', e.response.data.detail)
+        })
+    })
+    this.connection.on('Logout', (questionId, score) => {
+      console.log("on('Logout)", questionId, score)
+      this.authenticated = 'Unauthenticated'
+      // questionHub.$emit('score-changed', { questionId, score })
+    })
   },
   methods: {
     ...mapActions('formManager', ['performFetchALlForms']),
     submitForm() {
       console.log('Form submitted! ->', this.userAnwers)
+      this.connection.invoke(
+        'SignMessage',
+        btoa(JSON.stringify(this.userAnwers))
+      )
+      this.waitingForSign = true
     },
     handleUserAnswerChange(questionId, answerId) {
       this.userAnwers[questionId] = answerId
