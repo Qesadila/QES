ad81784bd871729504a04121814e05dfcf3a8d8e
scholtz <ludko2@gmail.com>
2020-04-12 15:45:55 +0000

    Deploying to gh-pages from  @ 61db87fa1e7b67edfc5300b14852f14d46f00090 🚀

---
+++ _nuxt/bee335029a0bc983008f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{511:function(t,e,n){"use strict";n.r(e);var l={},c=n(60),component=Object(c.a)(l,(function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("h1",[this._v(this._s(this.$t("welcome")))])])}),[],!1,null,null,null);e.default=component.exports}}]);
