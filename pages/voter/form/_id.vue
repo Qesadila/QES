8b4f8cd804f5ea46f6a569ea11a6e7cabd249ea2
Ludovit Scholtz <ludovit.scholtz@wood.cz>
2020-04-13 13:53:40 +0200

    update signalr

--- pages/voter/form/_id.vue
+++ pages/voter/form/_id.vue
@@ -1,90 +1,100 @@
 <template>
-  <v-row no-gutters class="fill-height pa-12" justify="center" align="center">
-    <v-card v-if="selectedForm && appRunning" width="100%">
-      <form>
-        <div class="d-flex flex-row justify-center py-5 display-1">
-          {{ selectedForm.name }}
-        </div>
-        <div class="d-flex flex-row justify-center px-12 mb-6 body">
-          {{ selectedForm.infoText }}
-        </div>
-        <v-divider class="mb-10"></v-divider>
-        <div
-          v-for="(question, index) in selectedForm.votingFormItems"
-          :key="question.votingFormItemId"
-          class="px-12"
-        >
-          <div class="body-2 mb-2">Question {{ index + 1 }}</div>
-          <div class="title">{{ question.question }}</div>
-          <div class="pl-5">
-            <v-radio-group
-              @change="handleUserAnswerChange(question.question, $event)"
-            >
-              <v-radio
-                v-for="option in question.votingFormItemOptions"
-                :key="option.votingFormItemOptionId"
-                :label="option.option"
-                :value="option.option"
-                class="my-2"
-              >
-              </v-radio>
-              <!-- Last option is always with value null as user dont want to answer this question -->
-              <v-radio
-                class="my-2"
-                label="I do not want to answer"
-                value="N/A"
-              ></v-radio>
-            </v-radio-group>
-            <v-divider class="mb-10"></v-divider>
+  <div>
+    <v-alert :type="signalStatusType" v-if="signalStatusText">{{
+      signalStatusText
+    }}</v-alert>
+    <v-row no-gutters class="fill-height pa-12" justify="center" align="center">
+      <v-card v-if="selectedForm && appRunning" width="100%">
+        <form>
+          <div class="d-flex flex-row justify-center py-5 display-1">
+            {{ selectedForm.name }}
           </div>
-        </div>
+          <div class="d-flex flex-row justify-center px-12 mb-6 body">
+            {{ selectedForm.infoText }}
+          </div>
+          <v-divider class="mb-10"></v-divider>
+          <div
+            v-for="(question, index) in selectedForm.votingFormItems"
+            :key="question.votingFormItemId"
+            class="px-12"
+          >
+            <div class="body-2 mb-2">Question {{ index + 1 }}</div>
+            <div class="title">{{ question.question }}</div>
+            <div class="pl-5">
+              <v-radio-group
+                @change="handleUserAnswerChange(question.question, $event)"
+              >
+                <v-radio
+                  v-for="option in question.votingFormItemOptions"
+                  :key="option.votingFormItemOptionId"
+                  :label="option.option"
+                  :value="option.option"
+                  class="my-2"
+                >
+                </v-radio>
+                <!-- Last option is always with value null as user dont want to answer this question -->
+                <v-radio
+                  class="my-2"
+                  label="I do not want to answer"
+                  value="N/A"
+                ></v-radio>
+              </v-radio-group>
+              <v-divider class="mb-10"></v-divider>
+            </div>
+          </div>
 
-        <div class="d-flex flex-row justify-center px-12 mb-6 body">
-          <v-btn x-large color="primary" @click="submitForm">Send form</v-btn>
-        </div>
-      </form>
-    </v-card>
-
-    <div v-else>
-      <v-progress-circular indeterminate size="64"></v-progress-circular>
-    </div>
-
-    <v-dialog persistent :value="!appRunning" width="500">
-      <v-card>
-        <v-card-title class="headline grey lighten-2" primary-title>
-          Sign app not detected
-        </v-card-title>
-
-        <v-card-text class="pa-5">
-          Please start our sign app to procced on voting
-        </v-card-text>
-
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="warning" to="/voter">Go back</v-btn>
-        </v-card-actions>
-
-        <v-divider></v-divider>
+          <div class="d-flex flex-row justify-center px-12 mb-6 body">
+            <v-btn x-large color="primary" @click="submitForm">Send form</v-btn>
+          </div>
+        </form>
       </v-card>
-    </v-dialog>
 
-    <v-dialog persistent :value="waitingForSign" width="500">
-      <v-card>
-        <v-card-title class="headline grey lighten-2" primary-title>
-          Please sign your votes in app
-        </v-card-title>
-      </v-card>
-    </v-dialog>
-  </v-row>
+      <div v-else>
+        <v-progress-circular indeterminate size="64"></v-progress-circular>
+      </div>
+
+      <v-dialog persistent :value="!appRunning" width="500">
+        <v-card>
+          <v-card-title class="headline grey lighten-2" primary-title>
+            Sign app not detected
+          </v-card-title>
+
+          <v-card-text class="pa-5">
+            Please start our sign app to procced on voting
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer />
+            <v-btn color="warning" to="/voter">Go back</v-btn>
+          </v-card-actions>
+
+          <v-divider></v-divider>
+        </v-card>
+      </v-dialog>
+
+      <v-dialog persistent :value="waitingForSign" width="500">
+        <v-card>
+          <v-card-title class="headline grey lighten-2" primary-title>
+            Please sign your votes in app
+          </v-card-title>
+        </v-card>
+      </v-dialog>
+    </v-row>
+  </div>
 </template>
 <script>
 import { mapActions } from 'vuex'
-import { HubConnectionBuilder } from '@microsoft/signalr'
+const signalR = require('@microsoft/signalr')
 
 export default {
   middleware: 'authenticated',
   data() {
     return {
+      signalStatus: 'not-connected',
+      qaIdentity: '',
+      qaCertHash: '',
+      signalStatusType: 'warning',
+      signalStatusText: '',
       userAnwers: {},
       allLists: [],
       appRunning: false,
@@ -104,23 +114,35 @@ export default {
   mounted() {
     this.fetchList()
 
-    this.connection = new HubConnectionBuilder()
+    this.connection = new signalR.HubConnectionBuilder()
       .withUrl('http://localhost:58080/hubs/signin')
       .build()
 
-    this.connection
-      .start()
-      .then(() => {
-        this.appRunning = true
-      })
-      .catch((e) => {
-        console.log(e)
-      })
-
+    this.start()
+    /*
+    this.connection.onreconnecting(function() {
+      this.signalStatus = 'reconnecting'
+    })
+    this.connection.onreconnected(function() {
+      this.signalStatus = 'reconnected'
+    })
+    this.connection.ondisconnected(function() {
+      this.signalStatus = 'disconnected'
+      setTimeout(function() {
+        this.connection.start()
+      }, 5000) // Restart connection after 5 seconds.
+    })
     this.connection.on('Connecting', () => {
       alert('connecting')
     })
-
+    /**/
+    this.connection.on('Status', (status, identity, certHash) => {
+      this.signalStatus = status
+      this.qaIdentity = identity
+      this.qaCertHash = certHash
+      console.log('new status: ' + status)
+      this.setSignalRStatus()
+    })
     this.connection.on('Authenticate', (cert) => {
       const fd = new FormData()
       fd.append('base64message', cert)
@@ -140,12 +162,37 @@ export default {
         })
     })
     this.connection.on('Logout', (questionId, score) => {
-      console.log("on('Logout)", questionId, score)
       this.authenticated = 'Unauthenticated'
       // questionHub.$emit('score-changed', { questionId, score })
     })
+    this.setSignalRStatus()
   },
   methods: {
+    async start() {
+      try {
+        await this.connection.start()
+        console.assert(
+          this.connection.state === signalR.HubConnectionState.Connected
+        )
+        console.log('connected')
+        this.appRunning = true
+        this.getStatusFromServer()
+      } catch (err) {
+        console.assert(
+          this.connection.state === signalR.HubConnectionState.Disconnected
+        )
+        console.log(err)
+        setTimeout(() => this.start(), 5000)
+      }
+    },
+    async getStatusFromServer() {
+      try {
+        await this.connection.invoke('getStatus')
+      } catch (err) {
+        console.log(err)
+      }
+    },
+
     ...mapActions('formManager', ['performFetchALlForms']),
     submitForm() {
       console.log('Form submitted! ->', this.userAnwers)
@@ -164,6 +211,36 @@ export default {
     async fetchList() {
       const data = await this.performFetchALlForms()
       this.allLists = data
+    },
+    setSignalRStatus() {
+      switch (this.signalStatus) {
+        case 'not-connected':
+          this.signalStatusText = 'The website is not connected to the server'
+          this.signalStatusType = 'warning'
+          break
+        case 'identity-not-selected':
+          this.signalStatusText =
+            'Please select identity first in the QesadilaAuth'
+          this.signalStatusType = 'warning'
+          break
+        case 'identity-selected':
+          this.signalStatusText =
+            'Your current identity in QesadilaAuth: ' + this.qaIdentity
+          this.signalStatusType = 'info'
+          break
+        case 'reconnecting':
+          this.signalStatusText = 'reconnecting'
+          this.signalStatusType = 'warning'
+          break
+        case 'reconnected':
+          this.signalStatusText = 'reconnected'
+          this.signalStatusType = 'warning'
+          break
+        case 'disconnected':
+          this.signalStatusText = 'disconnected'
+          this.signalStatusType = 'warning'
+          break
+      }
     }
   }
 }
