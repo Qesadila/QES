4f5072c68c5b5f6a7f727774afde576cfde0d3a5
Ludovit Scholtz <ludko2@gmail.com>
2020-04-27 09:05:59 +0200

    update index, add video

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,47 @@
 <template>
   <div>
-    <div
-      v-html="$t('htmlCode.projectOverview')"
-      style="padding-left:20px;"
-    ></div>
+    <v-row>
+      <v-col>
+        <div
+          v-html="$t('htmlCode.projectOverview')"
+          style="padding-left:20px;"
+        ></div>
+      </v-col>
+      <v-col>
+        <div style="padding:20px;position:relative;">
+          <iframe
+            width="640"
+            height="400"
+            src="https://www.youtube.com/embed/e1LkfhTwZpI"
+          >
+          </iframe>
+          <h2>Fund raising</h2>
+          <p>
+            Please <b>donate money</b> to support this project to transparent
+            account
+            <a
+              href="https://ib.fio.sk/ib/transparent?a=2901594765&amp;l=ENGLISH"
+              >SK3783300000002901594765/FIOBCZPPXXX</a
+            >
+            owned by Srdcom Doma NGO.
+          </p>
+          <p>
+            We will use this money for further development and penetration
+            bounty system
+          </p>
+          <iframe
+            src="https://player.vimeo.com/video/406483646?byline=0&amp;portrait=0"
+            style="width:640px;height:400px"
+            frameborder="0"
+            allow="autoplay; fullscreen"
+            allowfullscreen
+          >
+          </iframe>
+        </div>
+        <script src="https://player.vimeo.com/api/player.js"></script>
+      </v-col>
+    </v-row>
     <span></span>
-    <div style="padding:62.5% 0 0 0;position:relative;">
-      <iframe
-        src="https://player.vimeo.com/video/406483646?byline=0&portrait=0"
-        style="position:absolute;top:0;left:0;width:640px;height:400px"
-        frameborder="0"
-        allow="autoplay; fullscreen"
-        allowfullscreen
-      >
-      </iframe>
-    </div>
-    <script src="https://player.vimeo.com/api/player.js"></script>
   </div>
 </template>
 
