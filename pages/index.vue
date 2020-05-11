1c617fb14b1d46cdcbde8f987d6ea80e7511e1bb
samuel@zubo.sk <jahoda76>
2020-05-11 09:02:14 +0200

    donation text CZ + explatation video code added

--- pages/index.vue
+++ pages/index.vue
@@ -13,25 +13,13 @@
       </v-col>
       <v-col>
         <div
-          style="padding:20px;position:relative;"
           v-html="$t('htmlCode.vimeoExplanationVideo')"
-        >
-          <h2>{{ $t('anonym.fundRaising') }}</h2>
-          <div
-            v-html="$t('htmlCode.fundRaising')"
-            style="padding:20px 0;"
-          ></div>
-          <iframe
-            width="100%"
-            height="400"
-            src="https://player.vimeo.com/video/406483646?byline=0&amp;portrait=0"
-            frameborder="0"
-            allow="autoplay; fullscreen"
-            allowfullscreen
-          >
-          </iframe>
-        </div>
-        <script src="https://player.vimeo.com/api/player.js"></script>
+          style="padding-left:20px;"
+        ></div>
+        <div
+          v-html="$t('htmlCode.fundRaising')"
+          style="padding-left:20px;"
+        ></div>
       </v-col>
     </v-row>
     <span></span>
